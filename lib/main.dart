c7608c071afb14ac686e88a5d6cf7e11eb8807bc
rkyang <rkyang@outlook.com>
2026-01-29 17:18:44 +0800

    d2 6 scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,22 @@ void main(List<String> args) {
   runApp(
     MaterialApp(
       title: 'flutter',
-      theme: ThemeData(scaffoldBackgroundColor: Colors.blue),
-      home: Scaffold()
+      // theme: ThemeData(scaffoldBackgroundColor: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Head'),
+          centerTitle: true
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Text('Center'),
+        ),
+        bottomNavigationBar: Container(
+          height: 80,
+          alignment: Alignment.center,
+          child: Text('Bottom'),
+        ),
+      )
     ),
   );
 }
