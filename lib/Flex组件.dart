90e04b644f9147bd9a12df9c84aa6915571faa58
Rkyang <rkyang@outlook.com>
2026-01-31 12:13:09 +0800

    d2 20 弹性布局 Flex组件

---
+++ lib/Flex组件.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MainPage());
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Column组件'), centerTitle: true),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.amber,
+          child: Flex(
+            direction: Axis.vertical,
+            children: [
+              Container(height: 100, color: Colors.blue),
+              Expanded(child: Container(height: 100, color: Colors.blueGrey)),
+              Container(height: 100, color: Colors.red),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
