00816e8baecac4809c0e5b337372d32418d46a5d
rkyang <rkyang@outlook.com>
2026-02-25 11:24:07 +0800

    主页底部tab栏实现

--- lib/pages/main/index.dart
+++ lib/pages/main/index.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learning/pages/main/cart.dart';
+import 'package:flutter_learning/pages/main/category.dart';
+import 'package:flutter_learning/pages/main/home.dart';
+import 'package:flutter_learning/pages/main/mine.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -8,12 +12,71 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  // 定义下方tab栏资源数据
+  final List<Map<String, String>> _tabDataList = [
+    {
+      "icon": "lib/assets/ic_public_home_normal.png",
+      "icon_active": "lib/assets/ic_public_home_active.png",
+      'text': '首页',
+    },
+    {
+      "icon": "lib/assets/ic_public_pro_normal.png",
+      "icon_active": "lib/assets/ic_public_pro_active.png",
+      'text': '分类',
+    },
+    {
+      "icon": "lib/assets/ic_public_cart_normal.png",
+      "icon_active": "lib/assets/ic_public_cart_active.png",
+      'text': '购物车',
+    },
+    {
+      "icon": "lib/assets/ic_public_my_normal.png",
+      "icon_active": "lib/assets/ic_public_my_active.png",
+      'text': '我的',
+    },
+  ];
+
+  // 定义下方tab栏组件数据
+  List<BottomNavigationBarItem> _getTabWidget() {
+    return List.generate(_tabDataList.length, (int index) {
+      return BottomNavigationBarItem(
+        icon: Image.asset(_tabDataList[index]['icon']!, width: 40, height: 40),
+        activeIcon: Image.asset(
+          _tabDataList[index]['icon_active']!,
+          width: 40,
+          height: 40,
+        ),
+        label: _tabDataList[index]['text'],
+      );
+    });
+  }
+
+  // 定义tab栏激活项索引
+  int _currentTabIndex = 0;
+
+  // 定义tab对应的view视图组件
+  final List<Widget> _tabViews = [
+    HomeView(),
+    CategoryView(),
+    CartView(),
+    MineView(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Main"),
-        centerTitle: true,
+      body: SafeArea(child: IndexedStack(index: _currentTabIndex ,children: _tabViews)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _getTabWidget(),
+        currentIndex: _currentTabIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        showUnselectedLabels: true,
+        onTap: (value) {
+          setState(() {
+            _currentTabIndex = value;
+          });
+        },
       ),
     );
   }
