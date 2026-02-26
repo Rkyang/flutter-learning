201264881cb520badd671971d59e9857c9215466
rkyang <rkyang@outlook.com>
2026-02-26 17:40:33 +0800

    获取轮播图数据-定义常量

---
+++ lib/constants/index.dart
@@ -0,0 +1,14 @@
+/// 全局常量
+class GlobalConstants {
+  /// 请求基础地址
+  static const String baseUrl = 'https://meikou-api.itheima.net';
+  /// 请求超时时间
+  static const int timeOut = 10;
+  /// 请求响应成功状态码
+  static const String successCode = '1';
+}
+
+/// 请求路径常量
+class HttpUrlConstants {
+  static const String bannerList = '/home/banner';
+}
